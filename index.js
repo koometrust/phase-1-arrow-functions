92e02836c2a27db7901bbe23c1ed33637f442cf9
Koome <judekoome360@gmail.com>
2023-03-21 08:52:57 +0300

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,9 @@
+// index.js
+
+const divide = function(a,b){
+    return a/b;
+}
+console.log(divide(2000,100))
+
+const square =  square =>(square **2 )
+const add = (parameter1,parameter2) => parameter1 + parameter2;
